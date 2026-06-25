b69d224e3db6029015029802459d107b1fe55b69
locwara <lethanhloc2612004@gmail.com>
2026-06-25 12:35:24 +0700

    Add CSRF_TRUSTED_ORIGINS for Railway

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://cupdien-production.up.railway.app',
+    'https://*.up.railway.app',
+]
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
